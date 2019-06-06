17a22c5ba5a68d3cd3badd8f8557d1cc4c17252c
sofia.price <sofia.price@corelogic.com.au>
2019-06-06 11:28:08 +1000

    get a list of virtual labs at homepage.

--- wp-content/themes/twentynineteen/home.php
+++ wp-content/themes/twentynineteen/home.php
@@ -10,23 +10,38 @@
 
 <?php get_header(); ?>
 
-<?php
-$json = apnic_get_remote();
-// var_dump( $json );
-// die();
-$posts = $json['virtualLabs'];
-	
-foreach ($posts as $post) {	
+<div class="container">	
+
+	<?php
+	$json = apnic_get_remote();
+	// var_dump( $json );
+	// die();
+	$posts = $json['virtualLabs'];
 ?>
-  <div class="apnic-posts">
-      <div class="image-container"><img src="<?php echo $post['thumbnail']; ?>" />    </div>
-      <h4><?php echo $post['name']; ?></h4>
+	<div class="list-number"><p><span class="count"><?php echo count($posts); ?></span>&#32;items</p></div>
+	
+<?php
+	foreach ($posts as $post) {	
+	?>
+  <div class="apnic-post">
+  	<div class="img-wrap">
+  		<img src="<?php echo $post['thumbnail']; ?>" />
+  	</div>	      
+    <div class="text-wrap">
+    	<div class="title-lang">
+    		<h3><?php echo $post['name']; ?></h4>
+	      <div class="lang-duration">
+	    	<p><?php echo $post['language']; ?>
+      	<?php echo $post['formattedDuration']; ?></p></div>
+    	</div>
+    	
       <p><?php echo $post['description']; ?></p>
+    </div>      
   </div>
 
-<?php
-}
-?>
+	<?php
+	}
+	?>
 
-
+</div>
 <?php get_footer(); ?>
